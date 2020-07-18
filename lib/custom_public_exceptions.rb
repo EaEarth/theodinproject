e96b4442e8d14bf1f3ed5b5c53eb2f883fd01b91
Earth <earthtanapong@gmail.com>
2020-07-18 17:53:14 +0700

    add some changes to test git

--- lib/custom_public_exceptions.rb
+++ lib/custom_public_exceptions.rb
@@ -1,7 +1,7 @@
 class CustomPublicExceptions < ActionDispatch::PublicExceptions
   def call(env)
     status = env["PATH_INFO"][1..-1]
-
+    puts "some_change"
     if status == "404"
       Rails.application.routes.call(env)
     else
---
+++ lib/test_git.rb
@@ -0,0 +1,5 @@
+def test
+    puts "This is test!"
+end
+
+test
